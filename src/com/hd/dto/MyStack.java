40cecf351e6c03160097edf7d3f8a1c3b9751f71
赵传昊 <724356121@qq.com>
2018-10-21 00:31:45 +0800

    完善代码

--- src/com/hd/dto/MyStack.java
+++ src/com/hd/dto/MyStack.java
@@ -9,14 +9,19 @@ package com.hd.dto;
  */
 public interface MyStack<T> {
 
+    // 获取栈的大小
     int getSize();
 
+    // 压栈
     void push(T element);
 
+    // 出栈
     T pop();
 
+    // 获取栈顶元素
     T peek();
 
+    // 判断栈是否为空
     boolean isEmpty();
 
 }
